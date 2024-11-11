e86d211b41d68ccb6d6631decfc482a0e631875f
Erick <esantostaype@gmail.com>
2024-11-10 23:58:41 -0500

    remove back button

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,4 +1,4 @@
-import { BackHomeMain, Coupon } from '@/components'
+import { Coupon } from '@/components'
 
 export default function Home() {
   return (
@@ -7,9 +7,9 @@ export default function Home() {
         <h1 className="text-4xl md:text-6xl md:leading-tight mb-4 md:mb-8 font-bold bg-gradient-to-r to-accent1 from-accent2 bg-clip-text text-fill-transparent">Congratulations!</h1>
         <p>Congratulations on completing the survey! As a token of our appreciation, you’ve just earned a <strong className="text-xl bg-gradient-to-r to-accent1 from-accent2 bg-clip-text text-fill-transparent">25% discount</strong> on your next project with us. We’re thrilled to have your input, and we can’t wait to work with you!</p>
         <p>If you have any questions or would like more information about Dynamic Studio, feel free to visit our website.</p>
-        <div className="mt-10">
+        {/* <div className="mt-10">
           <BackHomeMain label='Back to Start' path='/'/>
-        </div>
+        </div> */}
       </div>
       <div className="flex-[0_0_300px] flex justify-end">
         <Coupon/>
